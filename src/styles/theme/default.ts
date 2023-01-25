32c013d4014109b5495299ebe33f6a5d528d0591
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-01-25 11:27:42 -0300

    Color blue-900

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,6 +1,7 @@
 export const defaultTheme = {
-    color: {
-        white: '#FFF',
-        blue: '#0D1117'
-    }
-};
+  color: {
+    white: '#FFF',
+    blue: '#0D1117',
+    'blue-900': '#161B22'
+  }
+}
