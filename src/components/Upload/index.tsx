82e9fca3703c78c323183a736bc07d2138e51b75
Luk4x <luk4xm4ci3l@gmail.com>
2023-02-15 10:33:26 -0300

    image fixed, uploader packege removed

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,9 +1,6 @@
-import { UploadContainer, ButtonUpload, Canvas } from './styled';
-
-import { Uploader } from 'uploader';
-import { UploadButton } from 'react-uploader';
 import { useState } from 'react';
-const uploader = Uploader({ apiKey: `${process.env.NEXT_PUBLIC_UPLOAD_API_KEY}` });
+
+import { UploadContainer, ButtonUpload, UserData } from './styled';
 
 interface UploadProps {
     bannerData: {
@@ -15,41 +12,30 @@ interface UploadProps {
 export function Upload({ bannerData }: UploadProps) {
     const [image, setImage] = useState<null | string>(null);
 
-    const handleImage = (image: any) => {
-        setImage(image[0].fileUrl);
-    };
-
     const convertToGif = () => {
-        const canvas: HTMLCanvasElement | null = document.querySelector('#canvas');
-        const ctx = canvas?.getContext('2d');
-        console.log(ctx);
-        if (ctx) {
-            ctx.font = '20px Roboto';
-            ctx.fillText(bannerData.presentation, 10, 50);
-            ctx.fillText(bannerData.office, 30, 70);
-        }
+        console.log('akame');
     };
 
     return (
         <>
-            <UploadContainer>
-                <Canvas image={image} id="canvas" />
-
-                {!image && (
-                    <UploadButton
-                        uploader={uploader}
-                        options={{ multi: false }}
-                        onComplete={files => handleImage(files)}
-                    >
-                        {({ onClick }) => (
-                            <ButtonUpload onClick={onClick}>Suba uma Imagem</ButtonUpload>
-                        )}
-                    </UploadButton>
+            <UploadContainer image={image}>
+                {!image ? (
+                    <ButtonUpload
+                        type="file"
+                        accept="image/*"
+                        onChange={e => {
+                            if (e.target.files !== null) {
+                                const bgImage = e.target.files[0];
+                                setImage(URL.createObjectURL(bgImage));
+                            }
+                        }}
+                    />
+                ) : (
+                    <UserData>
+                        <h1>{bannerData.presentation}</h1>
+                        <h2>{bannerData.office}</h2>
+                    </UserData>
                 )}
-                {/* <UserData>
-                            <h1>{bannerData.presentation}</h1>
-                            <h2>{bannerData.office}</h2>
-                        </UserData> */}
             </UploadContainer>
             <button
                 style={{ position: 'absolute', right: '-80px' }}
