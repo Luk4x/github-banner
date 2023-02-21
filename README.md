a5451ce65d2c62afb8cf6fca9c32ed4568a4781e
Lucas Maciel <luk4xm4ci3l@gmail.com>
2023-02-21 11:06:03 -0300

    data animated

--- README.md
+++ README.md
@@ -2,18 +2,16 @@
 
 ### etapas da MVP:
 
--   colocar layout do figma em prática ✔️;
--   upload da imagem ✔️;
--   preencher os dados da imagem conforme os inputs ✔️;
--   animar os dados da imagem ✔️;
--   exportar imagem como gif;
+- colocar layout do figma em prática ✔️;
+- upload da imagem ✔️;
+- preencher os dados da imagem conforme os inputs ✔️;
+- animar os dados ✔️;
+- exportar imagem;
 
 ### etapas extras:
 
--   melhorar o layout;
--   adicionar mais funcionalidades baseadas no site de referência;
--   adicionar mais temas;
--   melhorar o fluxo de upload de imagem;
--   explorar plataforma de upload: storaged files, account, plans, acceppted files
--   sigleton pattern <!-- https://vercel.com/guides/getting-started-with-nextjs-typescript-stripe -->
--   limitar qualidade da imagem (e redimensionar se necessário)
+- melhorar o layout;
+- adicionar mais funcionalidades baseadas no site de referência;
+- adicionar mais temas;
+- melhorar o fluxo de upload de imagem;
+- limitar qualidade da imagem (e redimensionar se necessário)
--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-
+import { useState, useRef } from 'react';
 import { UploadContainer, ButtonUpload, UserData } from './styled';
+import { saveAs } from 'file-saver';
 
 interface UploadProps {
     bannerData: {
@@ -10,15 +10,48 @@ interface UploadProps {
 }
 
 export function Upload({ bannerData }: UploadProps) {
+    const svgRef = useRef(null);
     const [image, setImage] = useState<null | string>(null);
 
     const convertToGif = () => {
-        console.log('akame');
+        if (svgRef.current !== null) {
+            const svg = svgRef.current;
+
+            //get svg source.
+            const serializer = new XMLSerializer();
+            let source = serializer.serializeToString(svg);
+
+            //add name spaces.
+            if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
+                source = source.replace(
+                    /^<svg/,
+                    '<svg xmlns="http://www.w3.org/2000/svg"'
+                );
+            }
+            if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
+                source = source.replace(
+                    /^<svg/,
+                    '<svg xmlns:xlink="http://www.w3.org/1999/xlink"'
+                );
+            }
+
+            //add xml declaration
+            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+            //convert svg source to URI data scheme.
+            const url = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(source);
+
+            //set url value to a element's href attribute.
+            const link = document.createElement('a');
+            link.download = 'Github-Banner.svg';
+            link.href = url;
+            link.click();
+        }
     };
 
     return (
         <>
-            <UploadContainer image={image}>
+            <UploadContainer image={null}>
                 {!image ? (
                     <ButtonUpload
                         type="file"
@@ -31,14 +64,52 @@ export function Upload({ bannerData }: UploadProps) {
                         }}
                     />
                 ) : (
-                    <UserData>
-                        <h1>{bannerData.presentation}</h1>
-                        <h2>{bannerData.office}</h2>
-                    </UserData>
+                    <svg
+                        width="800"
+                        height="350"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                        version="1.1"
+                        ref={svgRef}
+                    >
+                        {image && <image href={image} width="100%" />}
+                        <path id="path">
+                            <animate
+                                attributeName="d"
+                                from="m0,110 h0"
+                                to="m0,110 h1100"
+                                dur="6.8s"
+                                begin="0s"
+                                repeatCount="indefinite"
+                            />
+                        </path>
+                        <text
+                            font-size="28"
+                            font-family="Montserrat"
+                            fill="hsla(36, 95%, 85%, 1)"
+                            textAnchor="middle"
+                            x="50%"
+                            dy="4%"
+                        >
+                            <textPath alignmentBaseline="middle" xlinkHref="#path">
+                                {bannerData.presentation}
+                            </textPath>
+                        </text>
+                        <text
+                            font-size="22"
+                            font-family="Montserrat"
+                            fill="hsla(36, 95%, 85%, 1)"
+                            textAnchor="middle"
+                            x="50%"
+                            dy="14%"
+                        >
+                            <textPath xlinkHref="#path">{bannerData.office}</textPath>
+                        </text>
+                    </svg>
                 )}
             </UploadContainer>
             <button
-                style={{ position: 'absolute', right: '-80px' }}
+                style={{ position: 'absolute', right: '-80px', top: '-40px' }}
                 onClick={convertToGif}
             >
                 Download
@@ -46,5 +117,3 @@ export function Upload({ bannerData }: UploadProps) {
         </>
     );
 }
-
-// alert(files.map(x => x.fileUrl).join('\n'))
